8bf6cb7e01eff339ad8e2f64a3581a74feb2e334
Cao Gaoting <caogtaa@gmail.com>
2021-04-13 12:56:08 +0800

    rebuild as debug

---
+++ src/settings.920c6.js
@@ -0,0 +1,41 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "water",
+        "avatar",
+        "label"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/SceneWelcome.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "493b3",
+        main: "b9652"
+    }
+};
