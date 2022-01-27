1e699ecb8558a7bdd1f580ac34a73e362e927866
Cao Gaoting <caogtaa@gmail.com>
2022-01-27 17:03:10 +0800

    update SceneXOR

---
+++ src/settings.07d41.js
@@ -0,0 +1,42 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "water",
+        "avatar",
+        "label"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/SceneLoad.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "493b3",
+        resources: "5666a",
+        main: "16541"
+    }
+};
